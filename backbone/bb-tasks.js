2a9433632d0bb2794d274d509f7847bac7649a5b
Ville Anttonen <ville.anttonen@gmail.com>
2013-06-02 01:08:44 +0300

    shitty board

--- backbone/bb-tasks.js
+++ backbone/bb-tasks.js
@@ -1,5 +1,7 @@
 var Task = Backbone.Model.extend({
 
+  idAttribute: "_id",
+
 	defaults : {
 		'trail' : 'inbox'
 	},
@@ -35,10 +37,33 @@ var AddTaskView = Backbone.View.extend({
 
     add : function (){
     	var task = new Task();
-        task.set('name',this.$namefield.val())
+      task.set('name',this.$namefield.val())
     	this.collection.create(task);
     	this.$namefield.val('')
     }
     
 })
 
+var ShowTasksView = Backbone.View.extend({
+
+    initialize : function (opts){
+      this.listenTo(this.collection,"add change",this.render);
+      this.trail = opts.trail;
+      this.$el.empty();
+    },
+
+    template : _.template('<p><%= name %></p>'), 
+
+    render : function (){
+        this.$el.empty();
+        console.log("render");
+        var self = this;
+        _.each(this.collection.filter(function (task){
+          return task.get("trail") === self.trail;
+        }), function (task){
+          self.$el.append(self.template(task.toJSON()));
+        })
+        
+    },
+
+})
